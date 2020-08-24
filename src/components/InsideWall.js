47b6890341520b3c16f0663dbf5173d4f1789be6
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-24 10:10:06 -0700

    Build inside space

--- src/components/InsideWall.js
+++ src/components/InsideWall.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-export default ({ x = 0, y = 0, z = 0, length = 0, width = 0, props }) => {
+export default ({
+    color = 'rgb(200, 200, 200)',
+    x = 0,
+    y = 0,
+    z = 0,
+    length = 0,
+    width = 0,
+    depth = 0,
+    props
+}) => {
     return (
         <mesh position={[0 + x, 0 + y, 0 + z]} {...props}>
-            <boxBufferGeometry attach="geometry" args={[1, width, length]} />
+            <boxBufferGeometry
+                attach="geometry"
+                args={[depth, width, length]}
+            />
             <meshStandardMaterial
                 attach="material"
-                color="black"
+                color={color}
                 roughness={0.5}
             />
         </mesh>
--- src/views/Inside.js
+++ src/views/Inside.js
@@ -6,23 +6,132 @@ import styled from 'styled-components';
 
 import ViewLayer from '../components/ViewLayer';
 import Controls from '../components/Controls';
-import Wall from '../components/InsideWall';
+import Block from '../components/InsideWall';
+
+// For readability
+const Wall = Block;
+const Pedestal = Block;
 
 export default () => {
     return (
-        <ViewLayer backgroundColor="rgb(0, 0, 205)" zIndex={600}>
+        <ViewLayer backgroundColor="rgb(140, 140, 255)" zIndex={600}>
             <BackLink to="/">Back</BackLink>
-            <span>
-                <Canvas
-                    shadowMap
-                    camera={{
-                        position: new Vector3(5, 0, 0)
-                    }}
-                >
-                    <Wall x={-20} y={0} z={0} length={50} width={30} />
-                    <Controls />
-                </Canvas>
-            </span>
+            <Canvas
+                shadowMap
+                camera={{
+                    position: new Vector3(-10, 0, 0)
+                }}
+            >
+                <ambientLight intensity={0.5} />
+                <hemisphereLight
+                    color="rgb(100, 100, 100)"
+                    intensity={0.5}
+                    position={[10, 5, 0]}
+                />
+                <Wall
+                    color="rgb(0, 0, 205)"
+                    x={-60}
+                    y={0}
+                    z={0}
+                    length={70}
+                    width={30}
+                />
+                <Wall
+                    color="rgb(0, 0, 205)"
+                    x={60}
+                    y={0}
+                    z={0}
+                    length={70}
+                    width={30}
+                />
+                <Wall
+                    color="rgb(0, 0, 205)"
+                    x={0}
+                    y={15}
+                    z={0}
+                    length={70}
+                    depth={120}
+                />
+                <Wall
+                    color="rgb(0, 0, 205)"
+                    x={0}
+                    y={-15}
+                    z={0}
+                    length={70}
+                    depth={120}
+                />
+                <Wall
+                    color="rgb(0, 0, 205)"
+                    x={0}
+                    y={0}
+                    z={35}
+                    depth={120}
+                    width={30}
+                />
+                <Wall
+                    color="rgb(0, 0, 205)"
+                    x={0}
+                    y={0}
+                    z={-35}
+                    depth={120}
+                    width={30}
+                />
+                <Pedestal
+                    color="rgb(50, 50, 50)"
+                    x={30}
+                    y={-12}
+                    z={-10}
+                    depth={5}
+                    width={5}
+                    length={5}
+                />
+                <Pedestal
+                    color="rgb(50, 50, 50)"
+                    x={35}
+                    y={-12}
+                    z={0}
+                    depth={5}
+                    width={5}
+                    length={5}
+                />
+                <Pedestal
+                    color="rgb(50, 50, 50)"
+                    x={30}
+                    y={-12}
+                    z={10}
+                    depth={5}
+                    width={5}
+                    length={5}
+                />
+                <Pedestal
+                    color="rgb(50, 50, 50)"
+                    x={-30}
+                    y={-12}
+                    z={-10}
+                    depth={5}
+                    width={5}
+                    length={5}
+                />
+                <Pedestal
+                    color="rgb(50, 50, 50)"
+                    x={-35}
+                    y={-12}
+                    z={0}
+                    depth={5}
+                    width={5}
+                    length={5}
+                />
+                <Pedestal
+                    color="rgb(50, 50, 50)"
+                    x={-30}
+                    y={-12}
+                    z={10}
+                    depth={5}
+                    width={5}
+                    length={5}
+                />
+                <Controls />
+            </Canvas>
         </ViewLayer>
     );
 };
